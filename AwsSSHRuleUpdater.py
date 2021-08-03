640beff81d154ce352291a59210c925a314a2086
Joshua Holt <joshuaholtjobs@gmail.com>
2021-08-03 14:27:32 -0500

    Added ability to select from security groups connected to an instance

--- AwsSSHRuleUpdater.py
+++ AwsSSHRuleUpdater.py
@@ -3,25 +3,54 @@
 from requests import get
 import boto3
 from botocore.exceptions import ClientError
+import sys
 
-#Grabs public IP and adds CIDR
-NEW_IP = get('https://api.ipify.org').text + "/32"
-
-#Creates a connection to ec2
+#Creates a connection to ec2?
 ec2 = boto3.resource('ec2')
-client = boto3.client('ec2')
+client= boto3.client('ec2')
 
-#EC2 instance information
+#Describes ec2 instance information
 response = client.describe_instances()
-
-#Loops through everything in response and sets ID to first security group listed on EC2
+groups = []
 for r in response["Reservations"]:
 	for instance in r["Instances"]:
-		id = instance["SecurityGroups"][0]["GroupId"]
-		security_group = ec2.SecurityGroup(id)
+		for security_groups_temp in instance["SecurityGroups"]:
+			#print(security_groups_temp)
+			groups.append(security_groups_temp)
 
-#Adds current IP to SecurityGroup		
-response = security_group.authorize_ingress(IpProtocol = "TCP", CidrIp = NEW_IP, FromPort = 22, ToPort = 22)
+#Creates dictionary and gives user options
+i = 0
+pairs_dict = {}
+for grps in groups:
+	print(str(i) + ". " + grps['GroupName'])
+	pairs_dict[i]=grps
+	i += 1
+select_group = input("Which security group did you want to update? ")
 
-#Removes current IP from security group
-#response = security_group.revoke_ingress(IpProtocol = "TCP", CidrIp = NEW_IP, FromPort = 22, ToPort = 22)
+#Error checks user input
+try:
+	select_group = int(select_group)
+	#print(pairs_dict.get(select_group))
+except:
+	print("Needs to just be a number, re-run the script")
+	sys.exit()
+
+for r in response["Reservations"]:
+	for instance in r["Instances"]:
+		id = instance["SecurityGroups"][int(select_group)]["GroupId"]
+security_group = ec2.SecurityGroup(id)
+
+def add_rule():
+	NEW_IP = get('https://api.ipify.org').text + "/32"
+	response = security_group.authorize_ingress(IpProtocol = "TCP", CidrIp = NEW_IP, FromPort = 22, ToPort = 22)
+
+#def remove_old_rule():
+#	response = security_group.revoke_ingress(IpProtocol = "TCP", CidrIp = OLD_IP, FromPort = 22, ToPort = 22)
+
+Update_Rule = input("Are you sure you want to update the Security Group with your current IP? Type, \"Yes\" to update the Security Group: \n")
+
+if Update_Rule == "Yes":
+	add_rule()
+#	remove_old_rule()
+else:
+	print("Nothing was changed")
